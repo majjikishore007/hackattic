53eed7e7fc07037493f51fd23237ef8eb3ed1636
majjikishore007 <majjikishore031@gmail.com>
2022-11-20 22:13:47 +0530

    The images is not getting pushed into regestery

---
+++ DockerizedSolutions/index.js
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch';
+import shell from 'shelljs';
+
+const getData = async () => {
+  const response = await fetch(
+    'https://hackattic.com/challenges/dockerized_solutions/problem?access_token=ab17bddaef9e90e4'
+  );
+  const body = await response.json();
+  return body;
+};
+const push = async () => {
+  const res = await fetch(
+    'https://hackattic.com/challenges/dockerized_solutions/_/push/27e055b3.653a.448d.92ac.f4c623451d5b',
+    {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        registry_host: 'localhost:5000/v2',
+      }),
+    }
+  ).then((response) => {
+    //do something awesome that makes the world a better place
+    console.log(response);
+  });
+};
+const main = async () => {
+  // get the token and keys
+  //const data = await getData();
+  //const { credentials } = data;
+  //console.log('data', data);
+  // set up the authenation for the userr
+  // push pull the image to the reqestery
+  //{ user: 'gentle-smoke@hackattic.com', password: 'CPNMNY9C4H' },
+  /**
+     * data {
+  credentials: { user: 'gentle-smoke@hackattic.com', password: 'CPNMNY9C4H' },
+  ignition_key: 'OA8ZFIQMTJCUTFVHGPJFBIX5MUCK',
+  trigger_token: '27e055b3.653a.448d.92ac.f4c623451d5b'
+}
+     */
+  push();
+};
+
+main();
